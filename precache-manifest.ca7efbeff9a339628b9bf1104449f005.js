ed7dd4d4113afcaff4d644ab445f913c62bf58e5
RafaelBaptist <faelbaptista0@gmail.com>
2023-03-29 12:54:41 -0300

    Updates

---
+++ precache-manifest.ca7efbeff9a339628b9bf1104449f005.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e33c2066638f5a24fff",
+    "url": "/Portfolio/static/css/main.7e5156ae.chunk.css"
+  },
+  {
+    "revision": "1e33c2066638f5a24fff",
+    "url": "/Portfolio/static/js/main.1e33c206.chunk.js"
+  },
+  {
+    "revision": "89f8c12492339e6065e3",
+    "url": "/Portfolio/static/js/1.89f8c124.chunk.js"
+  },
+  {
+    "revision": "8a32220862b20c15eb26",
+    "url": "/Portfolio/static/css/2.c70d7433.chunk.css"
+  },
+  {
+    "revision": "8a32220862b20c15eb26",
+    "url": "/Portfolio/static/js/2.8a322208.chunk.js"
+  },
+  {
+    "revision": "1f733d32a014cd3041f7",
+    "url": "/Portfolio/static/js/runtime~main.1f733d32.js"
+  },
+  {
+    "revision": "7ec56b667b9684d46c7ab385821417f4",
+    "url": "/Portfolio/static/media/icons8-react-480.7ec56b66.png"
+  },
+  {
+    "revision": "445c76db2e605f940169c248dd7e4ad9",
+    "url": "/Portfolio/static/media/estrelas.445c76db.png"
+  },
+  {
+    "revision": "7716765e39672dc3ee989a45a7225a52",
+    "url": "/Portfolio/static/media/icons8-javascript-logo-480.7716765e.png"
+  },
+  {
+    "revision": "2336e9eb8272db4c8c489a85c0835ac9",
+    "url": "/Portfolio/static/media/icons8-angularjs-480.2336e9eb.png"
+  },
+  {
+    "revision": "e8f65c00c676dcf257e1c42ae6130b08",
+    "url": "/Portfolio/static/media/icons8-php-logo-480.e8f65c00.png"
+  },
+  {
+    "revision": "af93649fc1af8f2522ca7d8f08f5a2c4",
+    "url": "/Portfolio/static/media/museu.af93649f.png"
+  },
+  {
+    "revision": "9d9265ca2c9ae9715be921b632dd662e",
+    "url": "/Portfolio/static/media/red canids.9d9265ca.png"
+  },
+  {
+    "revision": "89254452362101fbd35cea466b4436b0",
+    "url": "/Portfolio/static/media/chal hotel.89254452.png"
+  },
+  {
+    "revision": "2399e82c7a7f3d85b51acc9444ac1adc",
+    "url": "/Portfolio/static/media/netfliz.2399e82c.png"
+  },
+  {
+    "revision": "715b8948fd452cb774ae2ac483961e2b",
+    "url": "/Portfolio/static/media/eu4direita.715b8948.png"
+  },
+  {
+    "revision": "3ed22ead4ee0e633c085883aed6cb943",
+    "url": "/Portfolio/static/media/euia2.3ed22ead.png"
+  },
+  {
+    "revision": "b090e3202375adb631519fab6bf121c2",
+    "url": "/Portfolio/static/media/RobotoMono-Medium.b090e320.ttf"
+  },
+  {
+    "revision": "d1a7b45f28bf337cab8adf3992f669a0",
+    "url": "/Portfolio/static/media/RobotoMono-Thin.d1a7b45f.ttf"
+  },
+  {
+    "revision": "0339b745f10bb01da181af1cdc33c361",
+    "url": "/Portfolio/static/media/RobotoMono-Bold.0339b745.ttf"
+  },
+  {
+    "revision": "802f1d483a93def35c4a47fb691ed638",
+    "url": "/Portfolio/static/media/wallpaperflare-cropped.802f1d48.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Portfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "8dddeaa9417c278c5b8dd1a6f1ab7c75",
+    "url": "/Portfolio/index.html"
+  }
+];
